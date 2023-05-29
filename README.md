6eb964236d29efb26acfb3d3d063f5d88c36f2a6
jsn <yx546@cornell.edu>
2023-05-28 20:23:34 -0400

    feat: add local vectorstore

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Python, LangChain, Supabase, HuggingFace, Kubernetes, Streamlit
    1. 前端其实只是保留所有的历史信息到messages列表，然后打印列表而已
    2. messages接收的query和answer都是字符串
    3. 因此后端返回的信息也是字符串，或者经过处理的字符串
+2. Supabase的使用
+   1. 前端state信息保存到supabse
+   2. 后端的vector信息保存到supabase
+      1. 首先实现本地的inject和持久化保存
 ## TODOs
 1. 环境配置：
    1. (ok) 改到Ubuntu上
@@ -14,7 +18,8 @@ Python, LangChain, Supabase, HuggingFace, Kubernetes, Streamlit
 2. 聊天场景：
    1. (ok) 添加记忆
    2. (ok) 添加预设的Prompt
-   3. 添加来源
+   3. (ok) pdf的批量上传
+   4. 添加来源
 3. 添加支持的输入类型
    1. txt
    2. 网页链接
--- src/utils/langbase.py
+++ src/utils/langbase.py
@@ -14,22 +14,43 @@ from utils.cal_tokens import get_num_tokens_from_messages
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+
 class Langbase:
     def __init__(self, openai_api_key=None) -> None:
-        OPENAI_API_KEY = openai_api_key
         self.embeddings = OpenAIEmbeddings(client=None, openai_api_key=openai_api_key)
-        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        self.memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
+        self.persist_directory = "./data/vectorstore"
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        self.memory = ConversationBufferMemory(
+            memory_key="chat_history", input_key="human_input"
+        )
         self.prompt = PromptTemplate(
             input_variables=["chat_history", "human_input", "context"],
             template="""You are a great expert.
                 context: {context}
                 chat_history: {chat_history}
                 Human: {human_input}
-                Expert:""")
-        self.llm = ChatOpenAI(client=None, model_name="gpt-3.5-turbo", temperature=0.9, openai_api_key=openai_api_key)
-        self.chain = None
-        self.db = None
+                Expert:""",
+        )
+        self.llm = ChatOpenAI(
+            client=None,
+            model_name="gpt-3.5-turbo",
+            temperature=0.9,
+            openai_api_key=openai_api_key,
+        )
+        if self.vectorstore_exists() == True:
+            self.db = Chroma(
+                persist_directory=self.persist_directory,
+                embedding_function=self.embeddings,
+            ).as_retriever()
+            self.chain = load_qa_chain(
+                self.llm, chain_type="stuff", memory=self.memory, prompt=self.prompt
+            )
+            pprint("Vectorstore exists.")
+        else:
+            self.db = None
+            self.chain = None
 
     def ask(self, question: str) -> str:
         if self.chain is None:
@@ -39,20 +60,28 @@ class Langbase:
         else:
             docs = self.db.get_relevant_documents(question)
             response = self.chain.run(input_documents=docs, human_input=question)
-            # pprint(response)
-        # return "answer"
         return response
 
     def ingest(self, file_path: str) -> None:
-        loader = PyMuPDFLoader(file_path)
-        documents = loader.load()
-        splitted_documents = self.text_splitter.split_documents(documents)
-        self.db = Chroma.from_documents(splitted_documents, self.embeddings).as_retriever()
-        self.chain = load_qa_chain(self.llm, chain_type="stuff", memory=self.memory, prompt=self.prompt)
-        # self.chain = load_qa_chain(self.llm, chain_type="stuff")
+        pdf_loader = PyMuPDFLoader(file_path)
+        loaded_documents = pdf_loader.load()
+        split_documents = self.text_splitter.split_documents(loaded_documents)
+        self.db = Chroma.from_documents(
+            documents=split_documents,
+            embedding=self.embeddings,
+            persist_directory=self.persist_directory,
+        ).as_retriever()
+        self.chain = load_qa_chain(
+            self.llm, chain_type="stuff", memory=self.memory, prompt=self.prompt
+        )
 
     def get_tokens(self, messages) -> int:
         return get_num_tokens_from_messages(messages)
+
+    def vectorstore_exists(self) -> bool:
+        if os.path.exists(self.persist_directory):
+            return len(os.listdir(self.persist_directory)) > 0
+        return False
 
     def forget(self) -> None:
         self.db = None
